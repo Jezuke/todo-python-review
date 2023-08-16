18c56808810e8448b20bac12b85763be6820bc36
Jezuke <jezaki48@gmail.com>
2023-08-15 21:58:37 -0700

    add task function added, list task function added but broken. need to fix

--- todo.py
+++ todo.py
@@ -11,7 +11,18 @@ Task:
  - id -> int
 """
 from os import system
+import uuid
 
+def print_options():
+    main_options = """\n**** OPTIONS ****
+1) add task
+2) list tasks
+3) delete task
+4) help
+5) quit
+*****************
+    """
+    print(main_options)
 class Task:
     def __init__(self, value, id) -> None:
         self._complete = False
@@ -24,41 +35,30 @@ class Task:
     def get_task(self):
         return self._value
 
+    def get_id(self):
+        return self._id
+    
     def set_complete(self):
         self._complete = True
 
     def set_incomplete(self):
         self._complete = False
 
-def switch(choice) -> int:
-    if choice == 1:
-        print("ADD ADD_TASK FN")
-    elif choice == 2:
-        print("ADD LIST_TASKS FN")
-    elif choice == 3:
-        print("ADD DELETE_TASKS FN")
-    elif choice == 4:
-        print_options()
-    elif choice == 5:
-        print("Quitting...")
-    else:
-        print("Invalid choice... Try again.")
-    return choice
-
 def clear():
     system("clear")
 
-def print_options():
-    main_options = """\n**** OPTIONS ****
-1) add task
-2) list tasks
-3) delete task
-4) help
-5) quit
-*****************
-    """
-    print(main_options)
+# TODO fix bug. After add one task, performing a list task, then trying to add another task, error gets thrown
+# Error: 'NoneType' object does not support item assignment
+def create_task():
+    task = input("Enter task: ")
+    print(f"Creating task: \"{task}\"", task)
+    task = Task(task, uuid.uuid1())
+    return task
 
+def list_tasks(tasks):
+    for task in tasks:
+        print("*", tasks[task].get_task())
+    
 def main():
     tasks = {}
 
@@ -68,9 +68,23 @@ def main():
 
     while choice != 5:
         try:
-            option = int(input("Choose an option: "))
-            choice = switch(option)
-        except:
+            choice = int(input("Choose an option: "))
+            if choice == 1:
+                task = create_task()
+                tasks[task.get_id()] = task
+            elif choice == 2:
+                print("Tasks:")
+                tasks = list_tasks(tasks)
+            elif choice == 3:
+                print("ADD DELETE_TASKS FN")
+            elif choice == 4:
+                print_options()
+            elif choice == 5:
+                print("Quitting...")
+            else:
+                print("Invalid choice... Try again.")
+        except Exception as e:
+            print(e)
             print("Invalid choice... Must be a number. Try again.")
     
     clear()
